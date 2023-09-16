36608e880c77c104adbf7a8f10695cca4b777a38
Nikolay-Kovalov <106543342+Nikolay-Kovalov@users.noreply.github.com>
2023-09-16 12:13:51 +0000

    Deploying to gh-pages from @ Nikolay-Kovalov/mySuperSite@44398a29c43687d2b10fa37b5ad256235487f62e 🚀

---
+++ index.29f53fbd.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault()}));
+//# sourceMappingURL=index.29f53fbd.js.map
---
+++ index.88787973.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault()}));
+//# sourceMappingURL=index.88787973.js.map
